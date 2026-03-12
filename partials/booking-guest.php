ba7f5605b50f41b986ada7cc77944443aee8cc09
Nicolas Lestrez <nicolas.lestrez@gmail.com>
2026-03-12 13:30:41 +0100

    fix: update login link in booking-guest.php

--- partials/booking-guest.php
+++ partials/booking-guest.php
@@ -2,7 +2,7 @@
     <h2>Accédez à la réservation</h2>
 
     <div class="tabs">
-        <a class="tabs-link" href="booking-guest.php">Connexion</a>
+        <a class="tabs-link" href="login.php">Connexion</a>
         <a class="tabs-link" href="register.php">Inscription</a>
     </div>
     <?php include 'partials/form-login.php'; ?>
